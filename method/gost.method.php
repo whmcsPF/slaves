aade951040103ff18be6dcf353625290038c297b
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-03 22:28:58 +0800

    Add files via upload

---
+++ method/gost.method.php
@@ -0,0 +1,40 @@
+<?php
+
+use PortForward\Base;
+
+class PortForward_gost
+{
+    public $rules;
+    public $b;
+    public function __construct()
+    {
+        $this->b = new Base();
+    }
+
+    public function PortForward_gost_create($data)
+    {
+        $this->b->run('nohup gost -L relay+ws://:'.$data['nodeport'].'/'.$data['forwardip'].':'.$data['forwardport'].' > /dev/null 2>&1&');
+        return true;
+    }
+
+    public function PortForward_gost_delete($data)
+    {
+        $this->b->run("kill $(ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']."/".$data['forwardip'].":".$data['forwardport']." | sed '/grep/d' | awk '{print $2}')");
+        return true;
+    }
+
+    public function PortForward_gost_olddelete($data)
+    {
+        $this->b->run("kill $(ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']."/".$data['forwardip'].":".$data['forwardport']." | sed '/grep/d' | awk '{print $2}')");
+        return true;
+    }
+
+    public function PortForward_gost_checkRepeat($data)
+    {
+        $res = $this->b->run("ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']."/".$data['forwardip'].":".$data['forwardport']." | sed '/grep/d' | awk '{print $2}'");
+        if (!empty(trim($res," "))) {
+            return true;
+        }
+        return false;
+    }
+}
