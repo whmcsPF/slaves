851420f393354f7ac19e5740c5cff3930eb155a9
xieruan <59883140+xieruan@users.noreply.github.com>
2021-08-03 23:12:14 +0800

    Update gost.method.php

--- method/gost.method.php
+++ method/gost.method.php
@@ -19,19 +19,19 @@ class PortForward_gost
 
     public function PortForward_gost_delete($data)
     {
-        $this->b->run("kill $(ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']."/".$data['forwardip'].":".$data['forwardport']." | sed '/grep/d' | awk '{print $2}')");
+        $this->b->run("kill $(ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
         return true;
     }
 
     public function PortForward_gost_olddelete($data)
     {
-        $this->b->run("kill $(ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']."/".$data['forwardip'].":".$data['forwardport']." | sed '/grep/d' | awk '{print $2}')");
+        $this->b->run("kill $(ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}')");
         return true;
     }
 
     public function PortForward_gost_checkRepeat($data)
     {
-        $res = $this->b->run("ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']."/".$data['forwardip'].":".$data['forwardport']." | sed '/grep/d' | awk '{print $2}'");
+        $res = $this->b->run("ps aux |grep gost\ -L\ relay+ws://:".$data['nodeport']." | sed '/grep/d' | awk '{print $2}'");
         if (!empty(trim($res," "))) {
             return true;
         }
